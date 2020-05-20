31447249eceb3a667bc7e9ffee5094a50c398e1f
GouMinghao <gmh2015@163.com>
2020-05-21 00:04:28 +0800

    update

--- examples/example1.py
+++ examples/example1.py
@@ -3,7 +3,7 @@ from Geometry3D import *
 from math import sqrt
 
 main_scene = naivert.Scene()
-main_camera = naivert.Camera(Point(300,-200,300),Point(280,-180,280),10 * Vector(-1 /sqrt(6),1/sqrt(6),2/sqrt(6)),10*Vector(1/sqrt(2),1/sqrt(2),0),'main_camera.png',resolution=(100,100))
+main_camera = naivert.Camera(Point(300,-200,300),Point(280,-180,280),10 * Vector(-1 /sqrt(6),1/sqrt(6),2/sqrt(6)),10*Vector(1/sqrt(2),1/sqrt(2),0),'main_camera.png',resolution=(1000,1000))
 # main_camera = naivert.Camera(Point(70,-100,50),Point(70,-50,50),Vector(0,0,40),Vector(40,0,0),'main_camera.png',resolution=(100,100))
 point_light = naivert.PointLight(Point(50,100,200),[5.0,5.0,5.0])
 point_light2 = naivert.PointLight(Point(200,50,200),[4.0,4.0,4.0])
@@ -15,9 +15,9 @@ main_scene.add_light(ambient_light)
 # main_scene.add_cph(Parallelepiped(origin(),100*x_unit_vector(),100*y_unit_vector(), 100 *z_unit_vector()),naivert.Material.DiffusionMaterial_White_1(),reverse_normal=True)
 main_scene.add_cph(Parallelepiped(Point(-50,-50,0),200*x_unit_vector(),200*y_unit_vector(), -z_unit_vector()),naivert.Material.DiffusionMaterial_White_1(),reverse_normal=True)
 main_scene.add_cph(Parallelepiped(Point(10,10,20),20*x_unit_vector(),20*y_unit_vector(), 40 *z_unit_vector()),naivert.Material.DiffusionMaterial_Red_1(),reverse_normal=False)
-main_scene.add_cph(Sphere(Point(70,30,40),20,30,10),naivert.Material.DiffusionMaterial_Blue_1(),reverse_normal=False)
-main_scene.add_cph(Cone(Point(70,70,0),10,80*z_unit_vector(),n=40),material=naivert.Material.DiffusionMaterial_Green_1(),reverse_normal=False)
-main_scene.add_cph(Cylinder(Point(30,70,0),15,80*z_unit_vector(),n=40),material=naivert.Material.SpecularMaterial_White_1(),reverse_normal=False)
+main_scene.add_cph(Sphere(Point(70,30,40),20,60,20),naivert.Material.DiffusionMaterial_Blue_1(),reverse_normal=False)
+main_scene.add_cph(Cone(Point(70,70,0),10,80*z_unit_vector(),n=80),material=naivert.Material.DiffusionMaterial_Green_1(),reverse_normal=False)
+main_scene.add_cph(Cylinder(Point(30,70,0),15,80*z_unit_vector(),n=80),material=naivert.Material.SpecularMaterial_White_1(),reverse_normal=False)
 # r = Renderer()
 # for face in main_scene.face_list:
 #     r.add((face.cpg,'r',1))
